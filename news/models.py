adc3b8ff681be8c32fe67bc3f7a1fca66ce24799
knxtdoor <knxtdoor@aol.com>
2021-08-20 16:45:55 -0400

    basic model tests for Article

--- news/models.py
+++ news/models.py
@@ -1,9 +1,28 @@
 from django.db import models
 from django.utils.text import slugify
+from django.utils import timezone
+
 
 from zeldaspeedruns import settings
 
 # Create your models here.
+
+
+class ArticleManager(models.Manager):
+    def create_article(self, title, content, author, pub_date):
+        if not title or not content or not author or not pub_date:
+            raise ValueError("Invalid article parameters")
+        if pub_date > timezone.now():
+            raise ValueError("Cannot be published in the future!")
+        article = self.model(
+            title=title,
+            content=content,
+            author=author,
+            pub_date=pub_date,
+            slug=(slugify(title))
+        )
+        article.save()
+        return article
 
 
 class Article(models.Model):
@@ -13,10 +32,7 @@ class Article(models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     pub_date = models.DateTimeField('date published')
     slug = models.SlugField(unique=True)
+    objects = ArticleManager()
 
     def __str__(self):
         return self.title
-
-    def save(self, *args, **kwargs):
-        self.slug = self.slug or slugify(self.title)
-        super().save(*args, **kwargs)
--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+import datetime
 
+from django.test import TestCase
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+from .models import Article
 # Create your tests here.
+
+
+class ArticleModelTests(TestCase):
+    test_username = 'testuser'
+    test_email = 'test@example.com'
+
+    def test_fail_with_no_arguments(self):
+        self.assertRaises(ValueError, Article.objects.create_article,
+                          title=None, content=None, author=None, pub_date=None)
+
+    def test_fail_with_missing_argument(self):
+        self.assertRaises(ValueError, Article.objects.create_article,
+                          title="Article 1", content="This is some content", author=None, pub_date=timezone.now())
+
+    def test_slug_creation(self):
+        u = get_user_model().objects.create_user(
+            username=self.test_username, email=self.test_email)
+        a = Article.objects.create_article(
+            title="Article with many words", content="This is some content", author=u, pub_date=timezone.now())
+        self.assertEqual(a.slug, "article-with-many-words")
+
+    def test_date_cant_be_future(self):
+        u = get_user_model().objects.create_user(
+            username=self.test_username, email=self.test_email)
+        self.assertRaises(ValueError, Article.objects.create_article,
+                          title="Article 1", content="Content", author=u, pub_date=timezone.now()+datetime.timedelta(days=1))
