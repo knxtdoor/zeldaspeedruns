367a6b6cfaf85005b73eb06be7296a0367e47a03
Zach Slaton <knxtdoor@aol.com>
2021-08-20 17:33:43 -0400

    merge 'newsfeed' into main
    * simple newsfeed and article detail view. Includes article model
    
    * fix typo that was preventing content from rendering
    
    * fix style issues and resolve formatting typo
    
    * basic model tests for Article
    
    * fix minor pr issues
    
    * fix timedelta thing

---
+++ news/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.utils.text import slugify
+from django.utils import timezone
+from django.conf import settings
+
+# Create your models here.
+
+
+class ArticleManager(models.Manager):
+    def create_article(self, title, content, author, pub_date):
+        if not title:
+            raise ValueError("Title must be supplied")
+        if not content:
+            raise ValueError("Content must be supplied")
+        if not author:
+            raise ValueError("Author must be supplied")
+        if not pub_date:
+            raise ValueError("Publish date must be supplied")
+        if pub_date > timezone.now():
+            raise ValueError("Cannot be published in the future")
+        article = self.model(
+            title=title,
+            content=content,
+            author=author,
+            pub_date=pub_date,
+            slug=(slugify(title))
+        )
+        article.save()
+        return article
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField('date published')
+    slug = models.SlugField(unique=True)
+    objects = ArticleManager()
+
+    def __str__(self):
+        return self.title
--- zeldaspeedruns/urls.py
+++ zeldaspeedruns/urls.py
@@ -20,6 +20,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('news.urls')),
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
     path('auth/', include('social_django.urls', namespace='social')),
